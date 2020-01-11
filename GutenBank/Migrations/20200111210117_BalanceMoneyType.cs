4d2c178fddae7bbb332daa4d185e656d39114660
ahmad <ahmad2smile@protonmail.com>
2020-01-12 02:36:43 +0500

    Added BalanceMoneyType migration

---
+++ GutenBank/Migrations/20200111210117_BalanceMoneyType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GutenBank.Migrations
+{
+    public partial class BalanceMoneyType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Balance",
+                table: "Accounts",
+                type: "Money",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Balance",
+                table: "Accounts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "Money");
+        }
+    }
+}

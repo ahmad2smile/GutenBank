f50326e30ee47ec3a2e748e3c8b0a6551fa70fee
ahmad <ahmad2smile@protonmail.com>
2020-01-13 18:51:16 +0500

    Setup n Applied SeedDataSetup Migration

--- GutenBank/Data/ApplicationContext.cs
+++ GutenBank/Data/ApplicationContext.cs
@@ -11,5 +11,24 @@ namespace GutenBank.Data
 
         public DbSet<Account> Accounts { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .HasData(
+                    new Account
+                    {
+                        AccountNumber = 1,
+                        Balance = 50,
+                        Currency = Currency.Dollar
+                    },
+                    new Account
+                    {
+                        AccountNumber = 2,
+                        Balance = 100,
+                        Currency = Currency.Dollar
+                    }
+            );
+        }
     }
 }
---
+++ GutenBank/Migrations/20200113134723_SeedDataSetup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GutenBank.Migrations
+{
+    public partial class SeedDataSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "AccountNumber", "Balance", "Currency" },
+                values: new object[] { 1, 50m, 0 });
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "AccountNumber", "Balance", "Currency" },
+                values: new object[] { 2, 100m, 0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountNumber",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountNumber",
+                keyValue: 2);
+        }
+    }
+}

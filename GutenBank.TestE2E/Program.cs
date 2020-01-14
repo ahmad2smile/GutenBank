c3d73555cf645110020e13642ad04fe3d30f37c6
ahmad <ahmad2smile@protonmail.com>
2020-01-14 17:25:12 +0500

    Setup Basic Console Proj for E2E Testing

---
+++ GutenBank.TestE2E/Program.cs
@@ -0,0 +1,54 @@
+﻿using GutenBank.TestE2E.Services;
+using Konsole;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GutenBank.TestE2E
+{
+    class Program
+    {
+        private static DataService _dataService = new DataService();
+
+        static async Task Main(string[] args)
+        {
+            var totalRequests = 3;
+
+            var tasks = new List<Task<HttpStatusCode>>();
+            var bar = new ProgressBar(PbStyle.DoubleLine, totalRequests);
+
+            for (int i = 0; i < totalRequests; i++)
+            {
+                // tasks.Add(_dataService.GetBalance());
+                tasks.Add(_dataService.PostDeposit());
+                //tasks.Add(_dataService.PostDeposit());
+            }
+
+            bar.Refresh(0, "");
+
+            var result = await Task.WhenAll(tasks.ToArray());
+
+            var success = 0;
+            var failed = 0;
+
+            foreach (var res in result)
+            {
+                if (res == HttpStatusCode.OK)
+                {
+                    success += 1;
+                    bar.Refresh(success, $"{success} Success of 300");
+                }
+                else
+                {
+                    Console.WriteLine(res.ToString());
+
+                    failed += 1;
+                }
+            }
+
+            Console.WriteLine($"{failed} Failed of 300");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GutenBank.TestE2E/Services/DataService.cs
@@ -0,0 +1,67 @@
+﻿using GutenBank.Domain;
+using GutenBank.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GutenBank.TestE2E.Services
+{
+    public class DataService
+    {
+        private readonly HttpClient _client;
+
+        public DataService()
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:49966/account/")
+            };
+        }
+
+        public async Task<HttpStatusCode> GetBalance(int? accountNumber = 1)
+        {
+            var response = await _client.GetAsync($"balance?accountNumber={accountNumber}");
+
+            return response.StatusCode;
+        }
+
+        public async Task<HttpStatusCode> PostDeposit(int accountNumber = 1, decimal amount = 100)
+        {
+            var transaction = new TransactionDTO
+            {
+                AccountNumber = accountNumber,
+                Amount = amount,
+                Currency = Currency.Dollar.ToString()
+            };
+
+            var json = JsonConvert.SerializeObject(transaction);
+
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync("deposit", data);
+
+            return response.StatusCode;
+        }
+
+        public async Task<HttpStatusCode> PostWithdraw(int accountNumber = 1, decimal amount = 100)
+        {
+            var transaction = new TransactionDTO
+            {
+                AccountNumber = accountNumber,
+                Amount = amount,
+                Currency = Currency.Dollar.ToString()
+            };
+
+            var json = JsonConvert.SerializeObject(transaction);
+
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync("withdraw", data);
+
+            return response.StatusCode;
+        }
+    }
+}
